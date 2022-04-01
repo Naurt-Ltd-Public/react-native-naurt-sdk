57e8f1bd8c9cd94dd519ebe77c6d8ca879086edd
NickS <nick.s@naurt.com>
2022-04-01 15:47:39 +0100

    chore: added android dependencies to gradle

--- android/src/main/java/com/reactnativenaurtsdk/NaurtSdkModule.kt
+++ android/src/main/java/com/reactnativenaurtsdk/NaurtSdkModule.kt
@@ -11,14 +11,5 @@ class NaurtSdkModule(reactContext: ReactApplicationContext) : ReactContextBaseJa
         return "NaurtSdk"
     }
 
-    // Example method
-    // See https://reactnative.dev/docs/native-modules-android
-    @ReactMethod
-    fun multiply(a: Int, b: Int, promise: Promise) {
-    
-      promise.resolve(a * b)
-    
-    }
-
     
 }
