0aa14a9444ecc1973367a85dcf2504e40c5914dd
AivaV <aiva.v@naurt.com>
2022-04-06 08:19:10 +0100

    chore: initial ios interop implementation

--- ios/NaurtSdk.m
+++ ios/NaurtSdk.m
@@ -1,9 +1,11 @@
-#import "React/RCTBridgeModule.h"
+#import <React/RCTBridgeModule.h>
 
 @interface RCT_EXTERN_MODULE(NaurtSdk, NSObject)
 
-RCT_EXTERN_METHOD(multiply:(float)a withB:(float)b
-                 withResolver:(RCTPromiseResolveBlock)resolve
-                 withRejecter:(RCTPromiseRejectBlock)reject)
+RCT_EXTERN_METHOD(initialiseNaurt:(String)apiKey (Int)precision)
+RCT_EXTERN_METHOD(startNaurt)
+RCT_EXTERN_METHOD(stopNaurt)
+RCT_EXTERN_METHOD(pauseNaurt)
+RCT_EXTERN_METHOD(resumeNaurt)
 
 @end
--- ios/NaurtSdk.swift
+++ ios/NaurtSdk.swift
@@ -3,13 +3,32 @@ import naurt_xcframework
 
 @objc(NaurtSdk)
 class NaurtSdk: NSObject {
-    @objc
-    static func requiresMainQueueSetup() -> Bool {
-      return true
-    }
-    
     @objc(multiply:withB:withResolver:withRejecter:)
     func multiply(a: Float, b: Float, resolve:RCTPromiseResolveBlock,reject:RCTPromiseRejectBlock) -> Void {
         resolve(a*b)
+    }
+    
+    @objc static func requiresMainQueueSetup() -> Bool {
+      return true
+    }
+    
+    @objc public func initialiseNaurt(apiKey: String, precision: Int) {
+      Naurt.shared.initialise(apiKey: apiKey, precision: precision);
+    }
+    
+    @objc public func startNaurt() {
+      Naurt.shared.start()
+    }
+    
+    @objc public func stopNaurt() {
+      Naurt.shared.stop()
+    }
+    
+    @objc public func pauseNaurt() {
+      Naurt.shared.pause()
+    }
+    
+    @objc public func resumeNaurt() {
+      Naurt.shared.resume()
     }
 }
